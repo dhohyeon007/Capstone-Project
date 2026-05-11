3fd15f3a223d3377729aeb0658d90b39cbffe90e
dhohyeon007 <dhohyeon007@gmail.com>
2026-05-12 01:23:23 +0900

    genai practice

---
+++ Gemini chatting bot.py
@@ -0,0 +1,38 @@
+from google import genai
+from google.genai import types
+import time
+
+client = genai.Client(api_key="AIzaSyA_Y3eq0u3QSXcShSPnJ57hPn7DTddVXdU")
+
+chat = client.chats.create(
+    model="gemini-2.5-flash",
+    config=types.GenerateContentConfig(
+        temperature=0.7
+    )
+)
+
+print("[ Gemini Response Bot ]")
+print("*" * 50)
+
+while True:
+    user_input = input("User: ")
+
+    if user_input.lower() == "q":
+        print("Exiting the chat. Goodbye!")
+        break
+
+    if not user_input.strip():
+        print("Please enter a valid message.")
+        continue
+
+    try:
+        response = chat.send_message(user_input)
+        print(f"Gemini: {response.text}")
+
+    except Exception as e:
+        if "503" in str(e):
+            print("waiting...")
+            time.sleep(5)
+            continue
+        else:
+            print(f"An error occurred: {e}")
