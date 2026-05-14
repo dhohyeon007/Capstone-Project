8545ea7503c38ee1b1e3348790cfcd49c78a85aa
dhohyeon007 <dhohyeon007@gmail.com>
2026-05-14 11:08:55 +0900

    2026-05-14

--- Capstone project.py
+++ Capstone project.py
@@ -1,6 +1,14 @@
 from google import genai
 from google.genai import types
+import time
 import os
+import sys
+import io
+
+os.environ["PYTHONUTF8"] = "1"
+if sys.stdout.encoding.lower() != "utf-8":
+    sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding="utf-8")
+    sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding="utf-8")
 
 client = genai.Client(api_key=os.environ.get('GOOGLE_API_KEY'))
 
