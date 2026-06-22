1465325e551c50800d9ee1da3f03ce2585f6a9c4
dhohyeon007 <dhohyeon007@gmail.com>
2026-06-22 16:21:42 +0900

    Feat: 마크다운 파일 네이밍 규칙 통일 및 페이로드 생성 함수 추가

--- New/pdf_preprocessing.py
+++ New/pdf_preprocessing.py
@@ -1,4 +1,6 @@
+import pymupdf as fitz
 import pymupdf4llm
+import re
 from pathlib import Path
 
 
@@ -14,7 +16,8 @@ def pdf_to_markdown(pdf_file_path):
         image_path=str(image_dir_path),
         show_progress=True
     )
-    text_file_path = text_dir_path / "Markdown.md"
+    page_count = len(fitz.open(pdf_file_path))
+    text_file_path = text_dir_path / f"Markdown-{1}-{page_count}.md"
     text_file_path.write_text(md_text, encoding="utf-8")
 
 
@@ -34,8 +37,42 @@ def pdf_to_markdown_chunks(pdf_file_path, chunk_size=15 ,overlap=2):
         end_page_num = min(i + chunk_size, page_count)
 
         md_chunk_text = "\n\n".join([md_page["text"] for md_page in md_pages[start_page_num:end_page_num]])
-        text_file_path = text_dir_path / f"Markdown_{start_page_num+1}-{end_page_num}.md"
+        text_file_path = text_dir_path / f"Markdown-{start_page_num+1}-{end_page_num}.md"
         text_file_path.write_text(md_chunk_text, encoding="utf-8")
 
         if end_page_num == page_count:
             break
+
+
+def construct_payload():
+    text_path_list = list(text_dir_path.iterdir())
+    image_path_list = list(image_dir_path.iterdir())
+    payload = []
+
+    if len(text_path_list) == 1:
+        # [[text, [images]]]
+        single_payload = []
+        single_payload.append(text_path_list[0])
+        single_payload.append(image_path_list)
+        payload.append(single_payload)
+        return payload
+    
+    else:
+        # [[text, [images]], [text, [images]], ...]
+        for text_path in text_path_list:
+            text_path_split = text_path.stem.split("-")
+            start_page_num = int(text_path_split[-2])
+            end_page_num = int(text_path_split[-1])
+            
+            matched_images = []
+            for image_path in image_path_list:
+                image_path_split = image_path.stem.split("-")
+                image_page_num = int(image_path_split[-2])
+                if start_page_num <= image_page_num <= end_page_num:
+                    matched_images.append(image_path)
+
+            chunk_payload = []
+            chunk_payload.append(text_path)
+            chunk_payload.append(matched_images)
+            payload.append(chunk_payload)
+        return payload
