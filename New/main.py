5dd2392586522e0dc72cf9d0379eed3a1d4a4d26
dhohyeon007 <dhohyeon007@gmail.com>
2026-06-25 01:11:35 +0900

    Fix: 병렬 파이프라인 실행 후 JSON 역직렬화 딕셔너리 반환

--- New/main.py
+++ New/main.py
@@ -1,6 +1,5 @@
 from preprocessing import *
-from llm import LLMCaller
-from orchestrator import *
+from chunk_processor import *
 from util import *
 import logging
 
@@ -16,24 +15,24 @@ def main():
 
     try:
         # 2. 임시 파일 디렉토리 생성
-        generate_dir()
+        data_dir_path, text_dir_path, image_dir_path, json_dir_path = generate_dir()
 
         # 3. 텍스트 및 이미지 추출
         # pdf_to_markdown(pdf_file_path)
-        pdf_to_markdown_chunks(pdf_file_path)
+        pdf_to_markdown_chunks(pdf_file_path, text_dir_path, image_dir_path)
 
         schema = load_json_schema()
 
-        orchestrator = ChunkProcessor(json_schema=schema)
+        chunk_processor = ChunkProcessor(schema, text_dir_path, image_dir_path, json_dir_path)
 
-        results = orchestrator.run_parallel_pipeline()
+        json_dict = chunk_processor.run_pipeline()
 
-        if results:
-            merged_result = orchestrator.merge_data(results)
+        print(json_dict)
+
     except Exception as e:
         logger.error("Terminating program.")
     # finally:
-    #     delete_dir()
+    #     delete_dir(data_dir_path)
 
 
 if __name__ == "__main__":
